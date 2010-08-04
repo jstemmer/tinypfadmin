c5cf7770c09c4cbd0f83cc14f476b415f7ca5eec
Joel Stemmer <stemmertech@gmail.com>
2010-08-04 21:07:14 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinypfadmin::Application.config.secret_token = '5662566430eeb8eb7eae070de1b445c8bbef4145d0e23c71427e17e2ab862dd1c5ed30d928b936fca5c15b2a63528b1bcd314c1263ddad5f74e3c950ef3cd07c'
