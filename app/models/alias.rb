b8c699f0066ba6e929e5ead6cdc3c07443caf360
Joel Stemmer <stemmertech@gmail.com>
2010-08-06 16:00:36 +0200

    update alias

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,3 +1,18 @@
 class Alias < ActiveRecord::Base
+  validates_presence_of :alias, :user
+  validate :alias_domain_exists
+
   belongs_to :user
+
+  def alias_domain
+    self.alias.split('@').last if self.alias && self.alias.include?('@')
+  end
+
+  def alias_name
+    self.alias.split('@').first if self.alias && self.alias.include?('@')
+  end
+
+  def alias_domain_exists
+    errors.add(:alias, 'uses an unknown domain') unless Domain.exists?(:domain => self.alias_domain)
+  end
 end
--- spec/models/alias_spec.rb
+++ spec/models/alias_spec.rb
@@ -1,15 +1,37 @@
 require 'spec_helper'
 
 describe Alias do
-  it "should belong to an existing user" do
-    pending
+  before(:each) do
+    Domain.create(:domain => "example.com")
+    user = User.create(:login => "user@example.com", :password => "example")
+
+    @alias = user.aliases.new(:alias => "mail@example.com")
   end
 
-  it "should have an alias" do
-    pending
+  it "is valid with valid attributes" do
+    @alias.should be_valid
+  end
+
+  it "is not valid without an alias" do
+    @alias.alias = nil
+    @alias.should_not be_valid
+  end
+
+  it "is not valid without a user" do
+    @alias.user = nil
+    @alias.should_not be_valid
   end
 
   it "should use an existing domain as part of the alias" do
-    pending
+    @alias.alias = "mail@example.org"
+    @alias.should_not be_valid
+  end
+
+  it "returns the domain part of the alias" do
+    @alias.alias_domain.should == "example.com"
+  end
+
+  it "returns the name part of the alias" do
+    @alias.alias_name.should == "mail"
   end
 end
