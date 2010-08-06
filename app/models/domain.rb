88031e1e53d3fea9b118db6e73b683634ccb2579
Joel Stemmer <stemmertech@gmail.com>
2010-08-06 14:49:55 +0200

    update domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,2 +1,9 @@
 class Domain < ActiveRecord::Base
+  validates_presence_of :domain
+  validates_format_of :domain, :with => /^[a-z0-9-]+\.[a-z0-9-]+$/
+  validates_uniqueness_of :domain, :case_sensitive => false
+
+  def domain=(d)
+    write_attribute(:domain, d.downcase)
+  end
 end
--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -2,24 +2,51 @@ require 'spec_helper'
 
 describe Domain do
   context "a new domain" do
-    it "should have a domain name" do
-      pending
+    before(:each) do
+      @domain = Domain.new
     end
 
-    it "should have a unique domain name" do
-      pending
+    it "should have a domain name" do
+      @domain.should_not be_valid
+      @domain.domain = 'example.com'
+      @domain.should be_valid
+    end
+
+    it "should have a unique domain name, case insensitive" do
+      Domain.create(:domain => 'example.com')
+
+      @domain.domain = 'example.com'
+      @domain.should_not be_valid
+
+      @domain.domain = 'EXAMPLE.com'
+      @domain.should_not be_valid
+
+      @domain.domain = 'example.org'
+      @domain.should be_valid
     end
 
     it "should only contain letters, digits and hyphens" do
-      pending
+      '!@#$%^&*()_+=/\\'.each_char do |d|
+        @domain.domain = d + 'example.com'
+        @domain.should_not be_valid
+      end
     end
 
     it "should contain a TLD" do
-      pending
+      @domain.domain = 'example'
+      @domain.should_not be_valid
+
+      @domain.domain = 'com'
+      @domain.should_not be_valid
     end
   end
 
   context "an existing domain" do
+    it "should store the domain in lowercase" do
+      @domain = Domain.create(:domain => 'Example.Com')
+      @domain.domain.should == 'example.com'
+    end
+
     it "should not be deleted while it has users or aliases" do
       pending
     end
