12e7cfc3497595d4e60b5f4f7837db124ba7ac22
Joel Stemmer <stemmertech@gmail.com>
2010-10-18 16:10:24 +0200

    fix domain view index spec

--- spec/views/domains/index.html.erb_spec.rb
+++ spec/views/domains/index.html.erb_spec.rb
@@ -4,16 +4,17 @@ describe "domains/index.html.erb" do
   before(:each) do
     assign(:domains, [
       stub_model(Domain,
-        :domain => "Domain"
+        :domain => "domain1.example.org"
       ),
       stub_model(Domain,
-        :domain => "Domain"
+        :domain => "domain2.example.org"
       )
     ])
   end
 
   it "renders a list of domains" do
     render
-    rendered.should have_selector("tr>td", :content => "Domain".to_s, :count => 2)
+    rendered.should have_selector("tr>td", :content => "domain1.example.org".to_s, :count => 1)
+    rendered.should have_selector("tr>td", :content => "domain2.example.org".to_s, :count => 1)
   end
 end
